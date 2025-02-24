3af5f5dd1947439d8b86cb3ccee8b11e96b382a7
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2025-02-24 16:21:12 +0800

    auto commit by win-upload.bat

---
+++ upload.sh
@@ -0,0 +1,6 @@
+echo '--------upload files start--------'   
+git add .
+git commit -m "auto update"
+echo '--------commit successfully--------'
+git push -u https://github.com/Shuaiwen-Cui/Research-Smart_Adaptive_Trigger_Sensing.git main
+echo '--------push to GitHub successfully--------'
